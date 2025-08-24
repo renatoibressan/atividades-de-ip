a5a3da44289d7cace7dd6c1d9bf0b6efc7ea7a75
Renato Bressan <renatoikeda98@gmail.com>
2025-08-24 13:05:55 -0300

    Add files via upload

---
+++ ex16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int abc, ab, a, b, c;
+    int cba;
+
+    // verifica se abc não é múltiplo de 10;
+    do {
+        // printf("Entre com o valor de abc: ");
+        scanf("%d", &abc);
+        if (abc % 10 == 0 || abc < 100 || abc > 999) {
+            printf("Erro\n");
+        }
+    } while (abc % 10 == 0 || abc < 100 || abc > 999);
+
+    // remove o último algarismo;
+    ab = abc / 10;
+    // obtém o algarismo do meio;
+    b = ab % 10;
+    // obtém o primeiro algarismo;
+    a = (ab - b)/10;
+    // obtém o último algarismo;
+    c = abc % 10;
+    // obtém o algarismo invertido;
+    cba = c * 100 + b * 10 + a;
+    printf("%d", cba);
+
+    return 0;
+}
