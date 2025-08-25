ad41ad6cbdf21a337d91ffad53aae99da178f608
Renato Bressan <renatoikeda98@gmail.com>
2025-08-25 09:18:39 -0300

    Update ex11.c

--- ex11.c
+++ ex11.c
@@ -12,15 +12,17 @@ int main() {
     // verifica se x está no range [0, 100];
     if (x < 0 || x > 100) {
         printf("Erro\n");
-    } return 0;
-
-    // printf("Entre com o valor em percentual dos impostos: ");
-    scanf("%f", &y);
-    // verifica se y está no range [0, 100];
-    if (y < 0 || y > 100) {
-        printf("Erro\n");
-    } return 0;
-
+        return 0;
+    } else {
+        // printf("Entre com o valor em percentual dos impostos: ");
+        scanf("%f", &y);
+        // verifica se y está no range [0, 100];
+        if (y < 0 || y > 100) {
+            printf("Erro\n");
+            return 0;
+        }
+    }
+    
     // calcula o custo final do carro;
     z = preco + (preco * (x/100)) + (preco * (y/100));
     printf("O VALOR DO CARRO E = %.2f\n", z);
