74faf3bf8c3cdaae4eb79b68f50e92e8165a7ea7
Renato Bressan <renatoikeda98@gmail.com>
2025-08-25 09:00:45 -0300

    Update ex11.c

--- ex11.c
+++ ex11.c
@@ -7,27 +7,24 @@ int main() {
     // printf("Entre com o valor do preço: ");
     scanf("%f", &preco);
 
+    // printf("Entre com o valor em percentual do distribuidor: ");
+    scanf("%f", &x);
     // verifica se x está no range [0, 100];
-    do {
-        // printf("Entre com o valor em percentual do distribuidor: ");
-        scanf("%f", &x);
-        if (x < 0 || x > 100) {
-            printf("Erro\n");
-        }
-    } while (x < 0 || x > 100);
+    if (x < 0 || x > 100) {
+        printf("Erro\n");
+    }
 
+    // printf("Entre com o valor em percentual dos impostos: ");
+    scanf("%f", &y);
     // verifica se y está no range [0, 100];
-    do {
-        // printf("Entre com o valor em percentual dos impostos: ");
-        scanf("%f", &y);
-        if (y < 0 || y > 100) {
-            printf("Erro\n");
-        }
-    } while (y < 0 || y > 100);
+    if (y < 0 || y > 100) {
+        printf("Erro\n");
+    }
 
     // calcula o custo final do carro;
     z = preco + (preco * (x/100)) + (preco * (y/100));
     printf("O VALOR DO CARRO E = %.2f\n", z);
 
     return 0;
+
 }
