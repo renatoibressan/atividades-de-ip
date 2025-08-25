75f0bd28145326cab2d19a9ca3268eef70d3bd23
Renato Bressan <renatoikeda98@gmail.com>
2025-08-25 02:56:07 -0300

    Update ex09.c

--- ex09.c
+++ ex09.c
@@ -3,20 +3,25 @@
 int main() {
     float n;
     int m;
+    double a1, a2, a3;
 
     // printf("Entre com o valor de n: ");
     scanf("%f", &n);
 
     // calcula o primeiro arredondamento;
     m = n * 10 + 0.5;
-    printf("%f\n", m / 10.0);
+    a1 = m / 10.0;
+    printf("%lf\n", a1);
     // calcula o segundo arredondamento;
     m = n * 100 + 0.5;
-    printf("%f\n", m / 100.0);
+    a2 = m / 100.0;
+    printf("%lf\n", a2);
     // calcula o terceiro arredondamento;
     m = n * 1000 + 0.5;
-    printf("%f\n", m / 1000.0);
+    a3 = m / 1000.0;
+    printf("%lf\n", a3);
 
     return 0;
 
 }
+
